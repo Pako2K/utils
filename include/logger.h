706c632ad3d5b38ccf40ccab14461697852842aa
Pako2K <41144260+Pako2K@users.noreply.github.com>
2018-08-24 22:45:50 +0200

    Small improvements

---
+++ include/logger.h
@@ -0,0 +1,74 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+
+#if !defined RELEASE_BUILD & !defined LIB_BUILD
+  #include <iostream>
+
+  #define INIT_LOG(x)
+  #define INIT_LOG_LEVEL(level, out)
+  #define DESTROY_LOG
+
+  #define DEBUG_LOG(trace)  std::cout << "DEBUG: " << trace << std::endl << std::endl
+  #define INFO_LOG(trace)   std::cout << "INFO: " << trace << std::endl << std::endl
+  #define ERROR_LOG(trace)  std::cout << "***********************************************************************" << std::endl \
+                                      << "ERROR: " << trace << std::endl \
+                                      << "***********************************************************************" << std::endl << std::endl
+#else
+  #define LEV_ERROR  utils::LogLevel::ERROR
+  #define LEV_INFO   utils::LogLevel::INFO
+  #define LEV_DEBUG  utils::LogLevel::DEBUG
+
+  #define INIT_LOG(out)               utils::Logger::initialize(out)
+  #define INIT_LOG_LEVEL(level, out)  utils::Logger::initialize(level, out)
+  #define DESTROY_LOG                 utils::Logger::destroy()
+
+  #define ERROR_LOG(trace) utils::Logger::log(utils::LogLevel::ERROR, trace)
+
+  #ifdef DEBUG_LOG_LEVEL
+    #define DEBUG_LOG(trace)  utils::Logger::log(utils::LogLevel::DEBUG, trace)
+    #define INFO_LOG(trace)   utils::Logger::log(utils::LogLevel::INFO, trace)
+  #elif defined INFO_LOG_LEVEL
+    #define DEBUG_LOG(trace)
+    #define INFO_LOG(trace)   utils::Logger::log(utils::LogLevel::INFO, trace)
+  #else
+    #define DEBUG_LOG(trace)
+    #define INFO_LOG(trace)
+  #endif // LOG_LEVEL
+
+  #include <iostream>
+  #include <fstream>
+  #include <string>
+  #include <map>
+
+  namespace utils
+  {
+    enum class LogLevel {DEBUG, INFO, ERROR};
+
+    class Logger
+    {
+    public:
+      static void initialize(const std::string &log_file_path);
+      static void initialize(std::ostream &out);
+      static void initialize(LogLevel level, const std::string &log_file_path) throw(std::string);
+      static void initialize(LogLevel level, std::ostream &out);
+
+      static void log(LogLevel level, std::string trace);
+
+      static void destroy();
+
+    private:
+      static Logger *_ptr;
+
+      std::map<int, std::ostream&> _level_map;
+
+      Logger(){}
+      ~Logger(){}
+    };
+
+  }
+
+#endif // RELEASE_TARGET
+
+
+#endif // LOGGER_H
---
+++ src/logger.cpp
@@ -0,0 +1,75 @@
+#include "logger.h"
+
+#include <typeinfo>
+
+using namespace utils;
+
+Logger *Logger::_ptr {nullptr};
+
+
+void Logger::initialize(const std::string &log_file_path) {
+  initialize(LogLevel::DEBUG, log_file_path);
+  initialize(LogLevel::INFO, log_file_path);
+  initialize(LogLevel::ERROR, log_file_path);
+}
+
+
+void Logger::initialize(std::ostream &out) {
+  initialize(LogLevel::DEBUG, out);
+  initialize(LogLevel::INFO, out);
+  initialize(LogLevel::ERROR, out);
+}
+
+
+void Logger::initialize(LogLevel level, const std::string &log_file_path) throw(std::string) {
+  try {
+    if ( _ptr && _ptr->_level_map.at(int(level)) )
+      return;
+  }
+  catch(...){
+  }
+
+  std::ofstream *log_file = new std::ofstream(log_file_path, std::ios::ate | std::ios::app);
+
+  if ( !log_file->is_open() )
+    throw std::string("Log File cannot be opened: " + log_file_path);
+
+  initialize(level, *log_file);
+}
+
+
+void Logger::initialize(LogLevel level, std::ostream &out) {
+  if ( !_ptr )
+    _ptr = new Logger();
+
+  _ptr->_level_map.insert(std::pair<int, std::ostream&>(int(level), out));
+}
+
+
+void Logger::log(LogLevel level, std::string trace) {
+  static const std::string HEADERS[] { "DEBUG: ", "INFO: ", "***********************************************************************\nERROR: "};
+  static const std::string TAILS[]   { "\n\n", "\n\n", "\n***********************************************************************\n\n"};
+  try {
+      _ptr->_level_map.at(int(level)) << HEADERS[int(level)] << trace << TAILS[int(level)];
+      _ptr->_level_map.at(int(level)).flush();
+  }
+  catch(...) {
+    std::cout << "Debug Level not initialized.";
+  }
+}
+
+
+void Logger::destroy() {
+  auto map_iter = _ptr->_level_map.begin();
+  while ( map_iter != _ptr->_level_map.end() )
+    if ( typeid((map_iter)->second) == typeid(std::ofstream) ) {
+      dynamic_cast<std::ofstream&>(map_iter->second).close();
+      delete &(map_iter++)->second;
+    }
+    else
+      map_iter++;
+
+  delete _ptr;
+}
+
+
